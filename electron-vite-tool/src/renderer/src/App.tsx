1622e830135022acdf1bde70eb25490deddd0e70
prajwal <prajwalkreddy17@gmail.com>
2025-07-19 19:02:55 +0530

    fix ts error for css files

--- electron-vite-tool/src/renderer/src/App.tsx
+++ electron-vite-tool/src/renderer/src/App.tsx
@@ -3,8 +3,16 @@ const App = () => {
 
   return (
     <>
-      <h1>hi how are you doing </h1>
-      <div>Hello world</div>
+      <div className="min-h-screen h-full w-full bg-black text-white">
+        <h1>hi how are you doing </h1>
+        <div className="bg-red-500">Hello world</div>
+        <p className="bg-blue-200">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid,
+          nisi rem? Corrupti atque aperiam voluptate tenetur blanditiis
+          voluptates temporibus accusantium obcaecati, magnam laborum velit
+          suscipit cum distinctio autem aspernatur rem.
+        </p>
+      </div>
     </>
   );
 };
